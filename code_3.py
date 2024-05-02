6987e19a21adf329bc84270ad653c17c3a33ce94
LikaShu <142459648+LikaShu@users.noreply.github.com>
2024-05-02 20:48:48 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,33 @@
+function main() {
+
+        console.log("Random Number: " + item);
+}
+    const data = [];
+}
+
+    return data;
+    });
+    }
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();    for (let i = 0; i < 10; i++) {
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+    }
+    data.forEach(item => {
+
+    });
+}
+function generateRandomData() {
+    const data = [];
+        console.log("Random Number: " + item);
+function main() {
+}
+    const data = generateRandomData();
